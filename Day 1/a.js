72c7ff4a56f73b87ce5aaa02d558c0221b92ea8c
Kevin Tatooles <tatooles@wisc.edu>
2023-12-02 13:17:48 -0600

    Day 1 part 1

--- Day 1/a.js
+++ Day 1/a.js
@@ -7,6 +7,27 @@ const data = await fsPromises.readFile(filename, "utf-8");
 
 const arr = data.split(/\r?\n/);
 
+let total = 0;
+
 for (const row of arr) {
-  console.log(row);
+  let firstNum;
+  let lastNum;
+
+  // Just go through twice, once starting from beginning, once from end
+  for (let i = 0; i < row.length; i++) {
+    if (!isNaN(parseInt(row[i]))) {
+      firstNum = row[i];
+      break;
+    }
+  }
+
+  for (let i = row.length - 1; i >= 0; i--) {
+    if (!isNaN(parseInt(row[i]))) {
+      lastNum = row[i];
+      break;
+    }
+  }
+  total += parseInt(firstNum.concat(lastNum));
 }
+
+console.log(total);
