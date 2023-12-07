53a784c213e5fb58a334dcac0b5b9495e24eea26
Kevin Tatooles <tatooles@wisc.edu>
2023-12-06 21:39:57 -0600

    Day 4 b finally

--- Day 4/b.ts
+++ Day 4/b.ts
@@ -2,19 +2,11 @@ const input = await Deno.readTextFile("./input.txt");
 
 const lines = input.split("\n");
 
-let total = 0;
+const workingList = [];
 
-const workingList = [...lines];
-
-const map = new Map();
-
-console.log(lines.length);
-
-for (const line of workingList) {
-  // console.log(line);
-  const number = parseInt(line.split(":")[0].split(" ")[1]) - 1;
+for (const line of lines) {
+  const number = parseInt(line.split(":")[0].replace("Card", ""));
   const split = line.split(":")[1].split("|");
-  // console.log(number, split);
 
   const winners = split[0]
     .trim()
@@ -32,20 +24,15 @@ for (const line of workingList) {
     if (winners.includes(num)) winCount++;
   });
 
-  // Now we nened logic to add the new rows
-  for (let i = 0; i < winCount; i++) {
-    // Need to parse out the number of this row
-    // console.log(lines[number + i + 1]);
-    const index = number + i + 1;
-    console.log(index);
-    if (index < lines.length - 1) {
-      workingList.push(lines[index]);
-    }
-  }
-
-  total++;
-
-  // console.log("winners", winners);
-  // console.log("actual", actual);
+  workingList.push({ number, winners, actual, winCount });
 }
-console.log(total);
+
+const cards = [...workingList];
+
+for (let i = 0; i < cards.length; i++) {
+  for (let j = 0; j < cards[i].winCount; j++) {
+    cards.push(workingList[cards[i].number + j]);
+  }
+}
+
+console.log(cards.length);
