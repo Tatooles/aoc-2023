a41f20ff7c314ff100c11101bec814faecf04ede
Kevin Tatooles <tatooles@wisc.edu>
2023-12-08 21:29:45 -0600

    Day 4 a code cleanup

--- Day 6/a.ts
+++ Day 6/a.ts
@@ -4,36 +4,36 @@ const lines = input.split("\n");
 
 let total = 1;
 
-let parsed = [];
+const parsed = [];
 
 // Parse input
 for (const line of lines) {
-  let nums = line
-    .split(":")[1]
-    .trim()
-    .split(" ")
-    .filter((x) => x !== "");
-
-  parsed.push(nums);
+  parsed.push(
+    line
+      .split(":")[1]
+      .trim()
+      .split(" ")
+      .filter((x) => x !== "")
+  );
 }
 
+// Run calculations
 for (let i = 0; i < parsed[0].length; i++) {
-  // Here is the calculation logic
   const duration = parseInt(parsed[0][i]);
   const record = parseInt(parsed[1][i]);
 
   let wins = 0;
-  // Calculate distance
-  // I think we have to loop all the way up until duration
+
+  // Loop all the way up until duration to hit all possible outcomes
   for (let j = 0; j < duration; j++) {
     let distance = 0;
-    const speed = j;
     const remainingDuration = duration - j;
-    distance = speed * remainingDuration;
+    distance = j * remainingDuration;
     if (distance > record) {
       wins++;
     }
   }
   total *= wins;
 }
+
 console.log(total);
