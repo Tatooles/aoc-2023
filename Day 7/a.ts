a9f71c38de02ae968ff31aa2226257009993c77d
Kevin Tatooles <tatooles@wisc.edu>
2023-12-14 23:13:43 -0600

    Create first check

--- Day 7/a.ts
+++ Day 7/a.ts
@@ -20,32 +20,71 @@ const cardStrength = new Map([
 
 let total = 0;
 
-lines.sort((a, b) => {
-  // Parse hand itself
-  const firstHand = a.split(" ")[0];
-  const secondHand = b.split(" ")[0];
+const main = () => {
+  const sorted = lines.sort((a, b) => {
+    // Parse hand itself
+    const firstHand = convertToMap(a.split(" ")[0]);
+    const secondHand = convertToMap(b.split(" ")[0]);
 
-  console.log(firstHand, secondHand);
-  // Implement sorting algo highest number returned at top
+    // Implement sorting algo highest number returned at top
 
-  // 5 of a kind
+    // 5 of a kind
+    const firstHas5 = check5(firstHand);
+    const secondHas5 = check5(secondHand);
+    console.log(firstHand, firstHas5);
+    if (firstHas5 === secondHas5) {
+      // Move on the next check
+    } else {
+      // Return whichever one is true
+      return firstHas5 ? -1 : 1;
+    }
 
-  // 4 of a kind
+    // 4 of a kind
 
-  // Full house
+    // Full house
 
-  // 3 of a kind
+    // 3 of a kind
 
-  // 2 pair
+    // 2 pair
 
-  // 1 pair
+    // 1 pair
 
-  // high card
+    // high card
 
-  // Else they are equal (only if exactly the same cards)
-  return 0;
-});
+    // Else they are equal (only if exactly the same cards)
+    return 0;
+  });
+  console.log(sorted);
+};
+
+const convertToMap = (hand: string) => {
+  const handMap = new Map<string, number>();
+
+  for (const char of hand) {
+    if (handMap.has(char)) {
+      handMap.set(char, handMap.get(char)! + 1);
+    } else {
+      handMap.set(char, 1);
+    }
+  }
+
+  return handMap;
+};
+
+const check5 = (hand: Map<string, number>) => {
+  // Check
+  for (const [key, value] of hand) {
+    if (value === 5) {
+      // found 5 of a kind
+      console.log("has 5 of a kind:", hand);
+      return true;
+    }
+  }
+  return false;
+};
 
 for (const line of lines) {
   // console.log(line);
 }
+
+main();
